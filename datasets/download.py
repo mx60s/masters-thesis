53f6490e4f98bd4e26f98525e9cd6703816fe3ff
Maggie von Ebers <vonebersm@gmail.com>
2025-01-23 14:17:24 -0600

    Update download.py

--- datasets/download.py
+++ datasets/download.py
@@ -1,7 +1,7 @@
 import subprocess
 
 url_prefix = (
-    "https://s3.us-west-1.wasabisys.com/predictive-coding-thesis/datasets/"
+    "https://thesis-github-files.s3.us-east-2.amazonaws.com/datasets/"
 )
 with open("files.txt", "r") as f:
     for line in f:
